5a7519623ba2b022e80f33104fb82623220c857d
Leonardo Lima Cavalcante <leonardo123k@gmail.com>
2021-04-10 23:40:06 -0300

    desafio 02 finalizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User does not exist!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length === 10) {
+    return response.status(403).json({ error: "Free todo's limit exceeded!" });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User does not exists!" });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "The todo id need to be a valid uuid!" });
+  }
+
+  const checkIfTodoExists = user.todos.find(todo => todo.id === id);
+
+  if (!checkIfTodoExists) {
+    return response.status(404).json({ error: "Todo does not exists!" });
+  }
+
+  request.user = user;
+  request.todo = checkIfTodoExists;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const checkIfUserExists = users.find(user => user.id === id);
+
+  if (!checkIfUserExists) {
+    return response.status(404).json({ error: "No user matching id" });
+  }
+
+  request.user = checkIfUserExists;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
